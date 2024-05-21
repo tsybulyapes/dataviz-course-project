2bf45d4cab306a1155e78dec66b685845db1057f
tsybulyapes <katerynaberezina@lnam.edu.ua>
2024-05-21 14:40:50 +0300

    build3

---
+++ dist/_app/immutable/entry/start.BAAfc_mP.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DZOSgKWJ.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,7 @@ const config = {
 			fallback: 'index.html' // makes error page
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : '/ukraine-map-demo'
+			base: process.argv.includes('dev') ? '' : '/dataviz-course-project'
 		}
 	}
 };
